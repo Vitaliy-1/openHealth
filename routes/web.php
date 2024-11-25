a9a96958841e8d396afa8d31db48cba0c37288db
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-11-25 17:38:59 +0200

    #70 Add the signing person with KEP key and adjust the property's name. Add validation for expiration date in documents.

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::middleware([
 
     Route::get('/dashboard/legal-entities/create', LegalEntities::class)->name('create.legalEntities');
 
-    Route::group(['middleware' => ['role:OWNER|ADMIN'],'prefix' => 'dashboard'], function () {
+    Route::group(['middleware' => ['role:OWNER|ADMIN'], 'prefix' => 'dashboard'], function () {
         Route::prefix('legal-entities')->group(function () {
             Route::get('/edit/{id?}', EditLegalEntity::class)->name('edit.legalEntities');
         });
@@ -86,7 +86,6 @@ Route::middleware([
         });
 
         Route::get('/test-license', [HomeController::class, 'test']);
-
     });
 
     Route::group(['middleware' => ['role:OWNER|ADMIN|DOCTOR']], function () {
