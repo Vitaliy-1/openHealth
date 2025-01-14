f4b18ee3f9b775699b8ce843d69c5614809b0b82
Vitalii Bezsheiko <vitaliybezsh@gmail.com>
2025-01-15 00:04:50 +0200

    Styling for employee form

--- resources/views/livewire/employee/_parts/_employee2.blade.php
+++ resources/views/livewire/employee/_parts/_employee2.blade.php
@@ -3,21 +3,34 @@
         {{__('forms.personalData')}}
     </legend>
 
-    <div class="form-row-4">
+    <div class="form-row-3">
         <div class="form-group group">
-            <input
-                   type="text"
-                   name="lastName"
-                   id="lastName"
-                   class="input peer"
-                   placeholder=" "
-                   required
-            />
-            <label
-                for="lastName"
-                class="label"
-            >
+            <input wire:model="employeeRequest.party.lastName" type="text" name="lastName" id="lastName" class="input peer" placeholder=" " required/>
+            <label for="lastName" class="label">
                 {{__('forms.lastName')}}
+            </label>
+        </div>
+
+        <div class="form-group group">
+            <input wire:model="employeeRequest.party.firstName" type="text" name="firstName" id="firstName" class="input peer" placeholder=" " required/>
+            <label for="firstName" class="label">
+                {{__('forms.firstName')}}
+            </label>
+        </div>
+
+        <div class="form-group group">
+            <input wire:model="employeeRequest.party.secondName" type="text" name="secondName" id="secondName" class="input peer" placeholder=" " required/>
+            <label for="secondName" class="label">
+                {{__('forms.secondName')}}
+            </label>
+        </div>
+    </div>
+
+    <div class="form-row-3">
+        <div class="form-group group">
+            <input wire:model="employeeRequest.party.birthDate" datepicker type="text" name="birthDate" id="birthDate" class="input default-datepicker peer" placeholder=" " required/>
+            <label for="birthDate" class="label">
+                {{__('forms.birthDate')}}
             </label>
         </div>
     </div>
