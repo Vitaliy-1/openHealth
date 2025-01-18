ba157cf9890e5a3d22a6a147083f2fcf2c2fd7f8
Vitalii Bezsheiko <vitaliybezsh@gmail.com>
2025-01-18 23:20:42 +0200

    Continue work on the styling

--- resources/views/livewire/employee/_parts/_employee2.blade.php
+++ resources/views/livewire/employee/_parts/_employee2.blade.php
@@ -54,6 +54,7 @@
                 id="secondName"
                 class="input peer @error('employeeRequest.party.secondName') input-error @enderror"
                 placeholder=" "
+                required
             />
             <label for="secondName" class="label">
                 {{__('forms.secondName')}}
@@ -68,6 +69,22 @@
     </div>
 
     <div class="form-row-3">
+        <div class="form-group group">
+            <label for="employeeGender" class="sr-only">{{__('forms.select')}} {{__('forms.gender')}}</label>
+            <select wire:model="employeeRequest.party.gender" id="employeeGender" class="input-select peer" required>
+                <option selected>{{__('forms.gender')}} *</option>
+                @foreach($this->dictionaries['GENDER'] as $k=>$gender )
+                    <option value="{{$k}}">{{$gender}}</option>
+                @endforeach
+            </select>
+
+            @error('employeeRequest.party.gender')
+            <p class="text-error">
+                {{$message}}
+            </p>
+            @enderror
+        </div>
+
         <div class="form-group group">
             <svg class="svg-input" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
@@ -91,6 +108,55 @@
                 <p class="text-error">
                     {{$message}}
                 </p>
+            @enderror
+        </div>
+
+        <div class="form-group group">
+            <input wire:model="employeeRequest.party.taxId"
+                   type="text"
+                   id="taxId"
+                   name="taxId"
+                   class="input peer @error('employeeRequest.party.taxId') input-error @enderror"
+                   placeholder=" "
+                   required
+            />
+
+            <label for="taxId" class="label">
+                {{__('forms.number')}} {{__('forms.RNOCPP')}}
+            </label>
+
+            @error('employeeRequest.party.taxId')
+                <p class="text-error">
+                    {{$message}}
+                </p>
+            @enderror
+        </div>
+    </div>
+
+    <div class="form-row-3">
+        <div class="form-group group">
+            <svg class="svg-input w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z"/>
+                <path d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z"/>
+            </svg>
+
+            <input
+                wire:model="employeeRequest.party.email"
+                type="text"
+                name="email"
+                id="email"
+                class="input peer @error('employeeRequest.party.email') input-error @enderror"
+                placeholder=" "
+                required
+            />
+            <label for="email" class="label">
+                {{__('forms.email')}}
+            </label>
+
+            @error('employeeRequest.party.email')
+            <p class="text-error">
+                {{$message}}
+            </p>
             @enderror
         </div>
     </div>
