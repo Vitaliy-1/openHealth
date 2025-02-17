89199ffaff3f87ddff210cef84f3c1cef1407316
Vitalii Bezsheiko <vitaliybezsh@gmail.com>
2025-02-17 23:02:16 +0200

    Create a modal and dropdown example

---
+++ app/Livewire/Employee/EmployeeEdit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire\Employee;
+
+class EmployeeEdit extends Employee
+{
+    public function render()
+    {
+        return view('livewire.employee.employee-edit');
+    }
+}
